3ba45f385adfed474d5a15e0e001bdb5e6f7e2bd
McZlik <github@mczlik.com>
2026-04-13 20:27:19 +0200

    Add invoke method with arguments

--- src/Libremidi.Net.Native/NativeMethods.cs
+++ src/Libremidi.Net.Native/NativeMethods.cs
@@ -1,6 +1,6 @@
-using System.Runtime.InteropServices;
-
 namespace Libremidi.Net.Native;
+
+using System.Runtime.InteropServices;
 
 internal static partial class NativeMethods
 {
@@ -8,7 +8,21 @@ internal static partial class NativeMethods
 
     // P/Invoke declarations will be added here as the libremidi C API is wrapped.
     // Reference: external/libremidi/include/libremidi/libremidi-c.h
-    
+
     [LibraryImport(LibName, EntryPoint = "libremidi_get_version", StringMarshalling = StringMarshalling.Utf8)]
     internal static partial string? GetVersion();
+
+    // const char* -> enum libremidi_api
+    [LibraryImport(
+        LibName,
+        EntryPoint = "libremidi_get_compiled_api_by_identifier",
+        StringMarshalling = StringMarshalling.Utf8)]
+    internal static partial LibremidiApi GetCompiledApiByIdentifier(string identifier);
+
+    // enum libremidi_api -> const char*
+    [LibraryImport(
+        LibName,
+        EntryPoint = "libremidi_api_display_name",
+        StringMarshalling = StringMarshalling.Utf8)]
+    internal static partial string? GetApiDisplayName(LibremidiApi api);
 }
--- src/Libremidi.Net/LibremidiInfo.cs
+++ src/Libremidi.Net/LibremidiInfo.cs
@@ -7,4 +7,17 @@ public static class LibremidiInfo
     static LibremidiInfo() => NativeLoader.EnsureLoaded();
     
     public static string Version => NativeMethods.GetVersion() ?? "Unknown";
+
+    public static bool TryGetApiDisplayName(string identifier, out string? displayName)
+    {
+        var api = NativeMethods.GetCompiledApiByIdentifier(identifier);
+        if (api == LibremidiApi.Unspecified)
+        {
+            displayName = null;
+            return false;
+        }
+        
+        displayName = NativeMethods.GetApiDisplayName(api);
+        return true;
+    }
 }
